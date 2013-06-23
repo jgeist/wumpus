4a4f341c7f431f3e2efd0e3e35b3acefd6a3a000
Jim Geist <jimge@rodentia.net>
2013-06-22 20:13:44 -0700

    More tests and methods on the map state object

--- javawumpus/src/WumpusMap.java
+++ javawumpus/src/WumpusMap.java
@@ -5,6 +5,8 @@ public class WumpusMap {
     private int wumpusPosition;
     private HashSet<Integer> pitPositions = new HashSet<Integer>();
     private HashSet<Integer> batPositions = new HashSet<Integer>();
+    private int batsAdded = 0;
+    private int pitsAdded = 0;
 
     public void setPlayerPosition(int p) {
         playerPosition = p;
@@ -24,6 +26,11 @@ public class WumpusMap {
     
     public void addBat(int p) {
         batPositions.add(p);
+        batsAdded++;
+    }
+    
+     public int batCount() {
+        return batPositions.size();
     }
     
     public boolean hasBatAt(int p) {
@@ -32,9 +39,91 @@ public class WumpusMap {
 
     public void addPit(int p) {
         pitPositions.add(p);
+        pitsAdded++;
     }
     
     public boolean hasPitAt(int p) {
         return pitPositions.contains(p);
+    }
+
+    public int pitCount() {
+        return pitPositions.size();
+    }
+
+    @Override
+    public Object clone() {
+        WumpusMap map = new WumpusMap();
+        
+        map.setPlayerPosition(playerPosition());
+        map.setWumpusPosition(wumpusPosition());
+        
+        for (Integer i : batPositions) {
+            map.addBat(i);
+        }
+
+        for (Integer i : pitPositions) {
+            map.addPit(i);
+        }
+
+        return map;
+    }
+    
+    @Override 
+    public boolean equals(Object other) {
+        if (other.getClass() != getClass()) {
+            return false;
+        }
+        
+        WumpusMap otherMap = (WumpusMap)other;
+        
+        if (playerPosition != otherMap.playerPosition || wumpusPosition != otherMap.wumpusPosition) {
+            return false;
+        }
+        
+        if (batPositions.size() != otherMap.batPositions.size() || pitPositions.size() != otherMap.pitPositions.size()) {
+            return false;
+        }
+        
+        for (Integer i : batPositions) {
+            if (!otherMap.batPositions.contains(i)) {
+                return false;
+            }
+        }
+        
+        for (Integer i : pitPositions) {
+            if (!otherMap.pitPositions.contains(i)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public boolean hasOverlap() {
+        if (batPositions.size() < batsAdded || pitPositions.size() < pitsAdded) {
+            return true;
+        }
+
+        if (playerPosition == wumpusPosition || hasBatAt(playerPosition) || hasPitAt(playerPosition)) {
+            return true;
+        }
+
+        if (hasBatAt(wumpusPosition) || hasPitAt(wumpusPosition)) {
+            return true;
+        }
+        
+        for (Integer i : batPositions) {
+            if (hasPitAt(i)) {
+                return true;
+            }
+        }
+        
+        for (Integer i : pitPositions) {
+            if (hasBatAt(i)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- javawumpus/test/WumpusMapTest.java
+++ javawumpus/test/WumpusMapTest.java
@@ -32,5 +32,76 @@ public class WumpusMapTest {
         assertEquals(false, map.hasPitAt(4));
         assertEquals(true, map.hasPitAt(5));
         assertEquals(true, map.hasPitAt(6));
+
+        assertEquals(false, map.hasOverlap());
+        
+        map.addPit(7);
+        assertEquals(2, map.batCount());
+        assertEquals(3, map.pitCount());
+        
+        assertEquals(true, map.equals(map));
     }
+
+    @Test
+    public void testOverlap() {
+        map.setPlayerPosition(2);
+        assertEquals(true, map.hasOverlap());
+
+        map.setPlayerPosition(3);
+        assertEquals(true, map.hasOverlap());
+
+        map.setPlayerPosition(4);
+        assertEquals(true, map.hasOverlap());
+
+        map.setPlayerPosition(5);
+        assertEquals(true, map.hasOverlap());
+
+        map.setPlayerPosition(6);
+        assertEquals(true, map.hasOverlap());
+
+        map.setPlayerPosition(1);
+        map.setWumpusPosition(3);
+        assertEquals(true, map.hasOverlap());
+
+        map.setWumpusPosition(4);
+        assertEquals(true, map.hasOverlap());
+
+        map.setWumpusPosition(5);
+        assertEquals(true, map.hasOverlap());
+
+        map.setWumpusPosition(6);
+        assertEquals(true, map.hasOverlap());
+        
+        map.setWumpusPosition(2);
+        assertEquals(false, map.hasOverlap());
+        
+        map.addBat(3);
+        assertEquals(true, map.hasOverlap());
+
+        map = new WumpusMap();
+        
+        map.setPlayerPosition(1);
+        map.setWumpusPosition(2);
+        map.addBat(3);
+        map.addBat(4);
+        map.addPit(3);
+        map.addPit(6);            
+        assertEquals(true, map.hasOverlap());
+    }
+
+    @Test
+    public void testClone() {
+        WumpusMap mapTwo = (WumpusMap)map.clone();
+
+        assertEquals(true, map.equals(mapTwo));
+    }
+
+    @Test
+    public void testEquals() {
+        WumpusMap mapTwo = (WumpusMap)map.clone();
+
+        assertEquals(true, map.equals(mapTwo));
+        mapTwo.addPit(42);
+        assertEquals(false, map.equals(mapTwo));
+    }    
 }
